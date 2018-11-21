e238668209599004fa9791558f677ee8d96046e1
unknown <cgrzegorz@gmail.com>
2018-11-21 22:46:42 +0100

    Done

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-var next = document.getElementById("nextPicture");
-var prev = document.getElementById("prevPicture");
+var next = document.getElementsByClassName("last-row");
+var prev = document.getElementsByClassName("first-row");
 
-var li = document.querySelectorAll(".slider li");
+var li = document.querySelectorAll(".sliders li");
 var numberImg = 0;
 
-li[numberImg].className = 'visible';
+li[numberImg].classList.add('visible');
 
-
-next.addEventListener("click", function (i) {
-   
+for(var i = 0 ; i < next.length ; i++){
+    next[i].addEventListener("click", function (i) {   
     
-    li[numberImg].classList.remove("visible");
+        li[numberImg].classList.remove("visible");
+        
+        numberImg++;
+        if (numberImg >= li.length) {
+            numberImg = 0;
+        }
+       li[numberImg].classList.add('visible');
     
-    numberImg++;
-    if (numberImg >= li.length) {
-        numberImg = 0;
-    }
-   li[numberImg].classList.add('visible');
-
-
-})
-
-prev.addEventListener("click", function (i) {
-    li[numberImg].classList.remove("visible");
-    numberImg--;
-    if (numberImg < 0) {
-        numberImg = li.length -1 ;
-    }
-    li[numberImg].classList.add('visible');
-})
+    
+    })
+    
+}
+for(var i = 0 ; i < next.length ; i++){
+    prev[i].addEventListener("click", function (i) {
+        li[numberImg].classList.remove("visible");
+        numberImg--;
+        if (numberImg < 0) {
+            numberImg = li.length -1 ;
+        }
+        li[numberImg].classList.add('visible');
+    })
+    
+}
